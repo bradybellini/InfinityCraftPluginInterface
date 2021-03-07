9e659e63b65dc97a99e8cba9a62b9d9136d06450
Brady Bellini <47389606+bradybellini@users.noreply.github.com>
2021-03-06 20:58:59 -0800

    initial

---
+++ src/main/java/com/playinfinitycraft/infinitycraft/InfinityCraft.java
@@ -0,0 +1,17 @@
+package com.playinfinitycraft.infinitycraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InfinityCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
